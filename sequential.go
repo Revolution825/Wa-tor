93e0131f77d135866cb2e96ab0d61ba2b86e085d
Revolution825 <diarmuidoneill2003@gmail.com>
2025-11-03 14:49:58 +0000

    Added movement logic for fish

--- sequential.go
+++ sequential.go
@@ -32,7 +32,7 @@ const height = 800
 var blue color.Color = color.RGBA{69, 145, 196, 255}
 var yellow color.Color = color.RGBA{255, 230, 120, 255}
 var red color.Color = color.RGBA{255, 50, 50, 255}
-var buffer [width][height]uint8 = [width][height]uint8{}
+var buffer [width][height]square = [width][height]square{}
 var count int = 0
 
 var numShark int = 0
@@ -40,23 +40,22 @@ var numFish int = 0
 var fishBreed int = 3
 var sharkBreed int = 8
 var starve int = 3
-var grid [width][height]uint8 = [width][height]uint8{}
+var grid [width][height]square = [width][height]square{}
 var threads int = 4
 
-type shark struct {
-	starveTimer int
-	breedTimer  int
+type square struct {
+	typeId int // 0 = empty space, 1 = fish, 2 = shark
+	energy int
 }
 
-type fish struct {
-	breedTimer int
-}
+var breedTimer int
+var chronon int = 1
 
 func frame(window *ebiten.Image) error {
 	count++
 	var err error = nil
 
-	if count == 1 {
+	if count == 100 {
 		err = update()
 		count = 0
 	}
@@ -68,15 +67,47 @@ func frame(window *ebiten.Image) error {
 }
 
 func update() error {
-	var err error = nil
 
 	for x := 0; x < width; x++ {
 		for y := 0; y < height; y++ {
-			buffer[x][y] = 0
-
+			buffer[x][y].typeId = 0
 		}
 	}
-	return err
+
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			if grid[x][y].typeId == 1 { // if fish
+				freeSquares := [][2]int{} // array of free square coordinates
+				if y > 0 && grid[x][y-1].typeId == 0 {
+					freeSquares = append(freeSquares, [2]int{x, y - 1})
+				}
+				if x > 0 && grid[x-1][y].typeId == 0 {
+					freeSquares = append(freeSquares, [2]int{x - 1, y})
+				}
+				if x < width-1 && grid[x+1][y].typeId == 0 {
+					freeSquares = append(freeSquares, [2]int{x + 1, y})
+				}
+				if y < height-1 && grid[x][y+1].typeId == 0 {
+					freeSquares = append(freeSquares, [2]int{x, y + 1})
+				}
+				if len(freeSquares) == 0 { // If there are no free squares, stay put
+					buffer[x][y].typeId = grid[x][y].typeId
+				} else { // If there are free squares, move to one at random
+					newPosition := rand.IntN(len(freeSquares))
+					buffer[freeSquares[newPosition][0]][freeSquares[newPosition][1]].typeId = 1
+				}
+
+			} else if grid[x][y].typeId == 2 { // if shark
+				//TODO: implement shark logic
+			}
+		}
+	}
+
+	chronon++
+	temp := buffer
+	buffer = grid
+	grid = temp
+	return nil
 }
 
 func display(window *ebiten.Image) {
@@ -88,9 +119,9 @@ func display(window *ebiten.Image) {
 		for y := 0; y < height; y++ {
 			for i := 0; i < scale; i++ {
 				for j := 0; j < scale; j++ {
-					if grid[x][y] == 1 {
+					if grid[x][y].typeId == 1 {
 						window.Set(x*scale+i, y*scale+j, yellow)
-					} else if grid[x][y] == 2 {
+					} else if grid[x][y].typeId == 2 {
 						window.Set(x*scale+i, y*scale+j, red)
 					}
 				}
@@ -106,11 +137,11 @@ func main() {
 		for y := 0; y < height; y++ {
 			// Float32() returns a random floating point number between 0.0 and 1.0
 			if rand.Float32() < 0.333 {
-				grid[x][y] = 1
+				grid[x][y].typeId = 1
 			} else if rand.Float32() < 0.5 {
-				grid[x][y] = 2
+				grid[x][y].typeId = 2
 			} else {
-				grid[x][y] = 0
+				grid[x][y].typeId = 0
 			}
 		}
 	}
